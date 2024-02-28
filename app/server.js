bb0e68b88d9201b8bbe7c95817a2e47a8675c438
ahme1kash <48543242+ahme1kash@users.noreply.github.com>
2024-02-28 16:35:12 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,13 +2,9 @@ require('dotenv').config()
 require('./config/db');
 const express = require('express')
 const app = express();
-// const path = require("path")
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-// app.set(path.join(__dirname, "views"))
-// app.set("view engine", "pug");
-// app.use('/public', express.static(path.join(__dirname, 'public')));
 const policyRoutes = require('./routers/policy_router');
 const claimsRoutes = require('./routers/claims_router');
 app.use('/policies', policyRoutes);
@@ -19,7 +15,6 @@ try{
       console.log(`Server is up and running at port ${port}`)
 
 })
-
 }
 catch(err){
     console.log(err.message)
